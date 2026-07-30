cb9f3e1c2bee7ecf17bac8292f7475074c7270cd
Renan Silva <renansilvaytB@gmail.com>
2026-07-30 12:00:24 -0300

    feat: estrutura inicial do comando com Mediatr

--- src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,23 @@
+using MediatR;
+
 namespace Catalog.Api.Products.CreateProduct;
 
-public class CreateProductHandler
+public record CreateProductCommand(
+  string Name,
+  string Description,
+  decimal Price,
+  List<string> Category,
+  string ImageFile
+)
+  : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
 {
+  public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+  {
+    // business logic to create a product
+    throw new NotImplementedException();
+  }
 }
