3cdea50f667737760afc84396131603e6c39a9dc
Renan Silva <renansilvaytB@gmail.com>
2026-09-17 19:56:46 -0300

    feat: adicionar modelos de domínio para pedidos, itens de pedidos, produtos, endereços e pagamentos

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -0,0 +1,10 @@
+namespace Ordering.Domain.ValueObjects;
+
+public record Address(
+  string FirstName,
+  string LastName,
+  string? EmailAddress,
+  string AddressLine,
+  string Country,
+  string State,
+  string ZipCode);
---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,8 @@
+namespace Ordering.Domain.ValueObjects;
+
+public record Payment(
+  string? CardName,
+  string CardNumber,
+  string Expiration,
+  string CVV,
+  int PaymentMethod);
