056127d58fb40ac4980cfa167309fba2887cddea
Renan Silva <renansilvaytB@gmail.com>
2026-09-17 19:33:38 -0300

    feat: adicionar abstrações de entidade e evento de domínio com MediatR

---
+++ src/Services/Ordering/Ordering.Domain/Abstraction/IDomainEvent.cs
@@ -0,0 +1,8 @@
+namespace Ordering.Domain.Abstraction;
+
+public interface IDomainEvent
+{
+    Guid EventId => Guid.NewGuid();
+    public DateTime OccurredOn => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName!;
+}
