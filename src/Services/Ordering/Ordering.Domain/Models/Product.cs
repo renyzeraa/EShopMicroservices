f1f13b2c7ec6f2360d25d88aa4eced689a2ff61c
Renan Silva <renansilvaytB@gmail.com>
2026-09-23 09:03:54 -0300

    feat: adicionar eventos de domínio para criação e atualização de pedidos, e refatorar a classe Order

--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -4,4 +4,17 @@ public class Product : Entity<ProductId>
 {
   public string Name { get; private set; } = default!;
   public decimal Price { get; private set; } = default!;
+
+  public static Product Create(ProductId id, string name, decimal price)
+  {
+    ArgumentException.ThrowIfNullOrWhiteSpace(name);
+    ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+    return new Product
+    {
+      Id = id,
+      Name = name,
+      Price = price
+    };
+  }
 }
