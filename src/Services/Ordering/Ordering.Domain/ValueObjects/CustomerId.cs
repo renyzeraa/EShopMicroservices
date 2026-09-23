f19c8e4822336cc8a0fa8a9a247e25bc1e52fdb8
Renan Silva <renansilvaytB@gmail.com>
2026-09-23 08:26:40 -0300

    feat: refatorar entidades e adicionar novos Value Objects para pedidos, itens de pedidos e produtos

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -0,0 +1,6 @@
+namespace Ordering.Domain.ValueObjects;
+
+public record CustomerId
+{
+  public Guid Value { get; }
+}
---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderItemId.cs
@@ -0,0 +1,6 @@
+namespace Ordering.Domain.ValueObjects;
+
+public record OrderItemId
+{
+  public Guid Value { get; }
+}
