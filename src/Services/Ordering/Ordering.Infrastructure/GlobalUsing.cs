84eefa225ac30a2a49d50484cc86ff5732334546
Renan Silva <renansilvaytB@gmail.com>
2026-09-24 08:22:24 -0300

    feat: adicionar configurações de entidades e contexto de banco de dados para pedidos, clientes, produtos e itens de pedidos

--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -1,2 +1,8 @@
+global using System.Reflection;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
+global using Ordering.Domain.Enums;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
