85295e481b9c541a28531c3276a11c41e975ae2e
Renan Silva <renansilvaytB@gmail.com>
2026-09-25 08:44:07 -0300

    fix: corrigir chamada de migração do banco de dados para uso síncrono

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -10,7 +10,7 @@ public static class DatabaseExtensions
 
         var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
-        await context.Database.MigrateAsync();
+        context.Database.MigrateAsync().GetAwaiter().GetResult();
 
         await SeedAsync(context);
     }
